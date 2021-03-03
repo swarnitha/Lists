507f8bb27a8241a2fe171591368e2edef6b5cb31
Swarnitha <swarnithakoda@gmail.com>
2021-03-03 14:49:38 +0530

    List and programs

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -3,7 +3,8 @@ object Example extends App {
   val mylist = List(1, 2, 3)
   val mylist2 = List("one", "two", "three");
   println(mylist.map(_ + 1))
-  val merge=(mylist zip mylist2).toMap
+
+  val merge = (mylist zip mylist2).toMap
   println(merge)
 
   val pair = (mylist.filter(_ % 2 == 0), mylist.filter(_ % 2 != 0))
